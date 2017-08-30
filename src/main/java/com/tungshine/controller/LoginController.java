822c6915dbb41b8aad85671a19801b11af5d93f4
justdoit <tangxushine@163.com>
2017-08-30 18:19:34 +0800

    1. SpringBoot项目初步搭建

---
+++ src/main/java/com/tungshine/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.tungshine.controller;
+
+/**
+ * Created by TungShine on 2017/8/30.
+ */
+public class LoginController {
+}
--- src/main/java/com/tungshine/springboot/Application.java
+++ src/main/java/com/tungshine/springboot/Application.java
@@ -2,11 +2,19 @@ package com.tungshine.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello";
+    }
 }
