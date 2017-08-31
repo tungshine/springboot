c31c41f29d48c4a08eb9380f124dbf8862589012
justdoit <tangxushine@163.com>
2017-08-31 17:29:43 +0800

    1. 添加swagger

--- src/main/java/com/tungshine/controller/LoginController.java
+++
@@ -1,7 +0,0 @@
-package com.tungshine.controller;
-
-/**
- * Created by TungShine on 2017/8/30.
- */
-public class LoginController {
-}
--- src/main/java/com/tungshine/springboot/Application.java
+++ src/main/java/com/tungshine/springboot/Application.java
@@ -2,19 +2,11 @@ package com.tungshine.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello";
     }
 }
